487431a4a54643d234af95e45b047aa7ab14333c
gunxroswar <gxz.gunforce@gmail.com>
2022-01-11 19:34:46 +0700

    "ทำแลป15เสร็จสิ้น"

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,38 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void inputMatrix(double a[][N]){
+	int i,j;
+	for(i=0;i<N;i++){
+		cout << "Row " << i+1 << ": ";
+		for(j=0;j<N;j++){
+			cin >> a[i][j];
+		}
+		
+	}
+}
+void showMatrix(const bool b[][N]){
+	int i,j;
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++){
+			if(b[i][j]==true) cout << "1 ";
+			else cout << "0 ";
+		}
+		cout << "\n";
+	}
+}
+void findLocalMax(const double a[][N], bool b[][N]){
+	int x,y;
+	for(x=0;x<N;x++){
+		for(y=0;y<N;y++){
+			if(a[x][y]<a[x][y+1] || a[x][y]<a[x][y-1] || a[x][y]<a[x+1][y] || a[x][y]<a[x-1][y]) 
+				b[x][y]=0;
+			else b[x][y]=1;
+			b[0][y]=0;
+			b[N-1][y]=0;			
+		}
+		b[x][0]=0;
+		b[x][N-1]=0;
+	}
+}	
